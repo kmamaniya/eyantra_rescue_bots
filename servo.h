9bd4821bee6e45ac0e5eb666088d6f7ee2ea839b
shauniop <rocks.shaun@gmail.com>
2017-02-17 19:29:40 +0530

    Init2

--- servo.h
+++ servo.h
@@ -1,19 +1,59 @@
+/*
+*
+* Project Name: 	e-Yantra Project
+* Author List: 		Karan Mamaniya
+* Filename: 		servo.h
+* Functions: 		servoPan_pin_config () , servoTilt_pin_config () , timer1_init () , servo_init () ,
+*					servoPan (unsigned char) , servoTilt(unsigned char) , servoPan_free () , servoTilt_free()
+* Global Variables:	NONE
+*
+*/
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
-//Configure PORTB 5 pin for servo motor 1 operation
+	/*
+	* Function Name:	servoPan_pin_config
+	* Input:			NONE
+	* Output:			NONE
+	* Logic:			DDRB(DIRECTION REGISTER B) SET as output
+						PORTB 5th bit IS SET TO LOGIC 1
+						Configure PORTB 5 pin for servo motor 1 operation
+	* Example Call:		servoPan_pin_config()
+	*
+	*/
+
 void servoPan_pin_config ()	{
+
 	DDRB = DDRB | 0x20;		//making PORTB 5 pin output
 	PORTB = PORTB | 0x20;	//setting PORTB 5 pin to logic 1
 }
 
-//Configure PORTB 6 pin for servo motor 2 operation
+
+	/*
+	* Function Name:	servoTilt_pin_config
+	* Input:			NONE
+	* Output:			NONE
+	* Logic:			DDRB(DIRECTION REGISTER B) SET as output
+						PORTB 6th bit IS SET TO LOGIC 1
+						Configure PORTB 6 pin for servo motor 2 operation
+	* Example Call:		servoTilt_pin_config()
+	*
+	*/
 void servoTilt_pin_config ()	{
 	DDRB = DDRB | 0x40;		//making PORTB 6 pin output
 	PORTB = PORTB | 0x40;	//setting PORTB 6 pin to logic 1
 }
 
+	/*
+	* Function Name:	timer1_init
+	* Input:			NONE
+	* Output:			NONE
+	* Logic:			Initializing timer 1 in 10 bit fast PWM Mode 
+	* Example Call:		servoTilt_pin_config()
+	*
+	*/
 void timer1_init ()	{
 	TCCR1B = 0x00; //stop
 	TCNT1H = 0xFC; //Counter high value to which OCR1xH value is to be compared with
@@ -33,18 +73,29 @@ void timer1_init ()	{
 	TCCR1B = 0x0C; //WGM12=1; CS12=1, CS11=0, CS10=0 (Prescaler=256)
 }
 
-//Initialize the ports
+	/*
+	* Function Name:	servo_init
+	* Input:			NONE
+	* Output:			NONE
+	* Logic:			Initialize the ports for the server motor operations 
+	* Example Call:		servo_init()
+	*
+	*/
 void servo_init ()	{
 	servoPan_pin_config(); //Configure PORTB 5 pin for servo motor 1 operation
 	servoTilt_pin_config(); //Configure PORTB 6 pin for servo motor 2 operation
 	timer1_init();
 }
+	
+	/*
+	* Function Name:	servoPan
+	* Input:			NONE
+	* Output:			NONE
+	* Logic:			Function to rotate Servo 1 by a specified angle 
+	* Example Call:		servoPan(unsigned char)
+	*
+	*/
 
-
-//Function to initialize all the peripherals
-
-
-//Function to rotate Servo 1 by a specified angle in the multiples of 1.86 degrees
 void servoPan (unsigned char degrees)	{
 	float PositionPanServo = 0;
 	PositionPanServo = ((float)degrees / 1.86) + 35.0;
@@ -53,7 +104,16 @@ void servoPan (unsigned char degrees)	{
 }
 
 
-//Function to rotate Servo 2 by a specified angle in the multiples of 1.86 degrees
+	
+	/*
+	* Function Name:	servoTilt
+	* Input:			NONE
+	* Output:			NONE
+	* Logic:			Function to rotate Servo 2 by a specified angle
+	* Example Call:		servoTilt(unsigned char)
+	*
+	*/
+
 void servoTilt(unsigned char degrees)	{
 	float PositionTiltServo = 0;
 	PositionTiltServo = ((float)degrees / 1.86) + 35.0;
@@ -61,14 +121,36 @@ void servoTilt(unsigned char degrees)	{
 	OCR1BL = (unsigned char) PositionTiltServo;
 }
 
-//servo_free functions unlocks the servo motors from the any angle 
-//and make them free by giving 100% duty cycle at the PWM. This function can be used to 
-//reduce the power consumption of the motor if it is holding load against the gravity.
+	/*
+	* Function Name:	servoPan_free
+	* Input:			NONE
+	* Output:			NONE
+	* Logic:			servo_free functions unlocks the servo motors from the any angle and make them free 
+						by giving 100% duty cycle at the PWM. This function can be used to reduce the power 
+						consumption of the motor if it is holding load against the gravity.
+						--->makes servo 1 free rotating
 
-void servoPan_free ()	{ //makes servo 1 free rotating
+	* Example Call:		servoPan_free()
+	*
+	*/
+
+void servoPan_free ()	{ 
 	OCR1AH = 0x03; 
 	OCR1AL = 0xFF; //Servo 1 off
 }
+
+	/*
+	* Function Name:	servoTilt_free
+	* Input:			NONE
+	* Output:			NONE
+	* Logic:			servo_free functions unlocks the servo motors from the any angle and make them free 
+						by giving 100% duty cycle at the PWM. This function can be used to reduce the power 
+						consumption of the motor if it is holding load against the gravity.
+						--->makes servo 2 free rotating
+
+	* Example Call:		servoTilt_free()
+	*
+	*/
 
 void servoTilt_free()	{ //makes servo 2 free rotating
 	OCR1BH = 0x03;
