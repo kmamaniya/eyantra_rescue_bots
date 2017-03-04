a720e999702055889c02b52d05c0fd9cdd9b84d6
kmamaniya <kmamaniya@gmail.com>
2017-03-04 12:42:26 +0530

    2.1.1

--- zigbee.h
+++ zigbee.h
@@ -4,11 +4,15 @@
 
 unsigned char serial_data; //to store received data from UDR1
 
+void clear_serial_data(){
+	serial_data = 0;
+}
+
 void start_sequence(){
 	while(1){
 		if(serial_data != 'a' || serial_data != 'A'){
 			lcd_string("Program started");
-			serial_data = 0;
+			clear_serial_data();
 			break;
 		}
 		_delay_us(500);
@@ -50,20 +54,16 @@ void transmit_array(uint16_t r[],unsigned char theta[], unsigned char phi[],int 
 
 void zigbee_init(void)
 {
-	 UCSR0B = 0x00; //disable while setting baud rate
-	 UCSR0A = 0x00;
-	 UCSR0C = 0x06;
-	 UBRR0L = 0x5F; //set baud rate lo
-	 UBRR0H = 0x00; //set baud rate hi
-	 UCSR0B = 0x98;
+	UCSR0B = 0x00; //disable while setting baud rate
+	UCSR0A = 0x00;
+	UCSR0C = 0x06;
+	UBRR0L = 0x5F; //set baud rate lo
+	UBRR0H = 0x00; //set baud rate hi
+	UCSR0B = 0x98;
 }
 
 SIGNAL(USART0_RX_vect) 		// ISR for receive complete interrupt
 {
 	serial_data = UDR0; 				//making copy of data from UDR0 in 'data' variable
 	//lcd_wr_char(serial_data);
-}
-
-void clear_serial_data(){
-	serial_data = 0;
 }
